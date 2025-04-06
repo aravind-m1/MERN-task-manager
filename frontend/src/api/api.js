b271ecbf939f454fb45ac2533fbec31c8df385de
Aravind <aravindmarripudi@gmail.com>
2025-04-06 20:57:59 +0530

    updated api again

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://mern-task-manager-73nz.onrender.com/api", 
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
