cb5da55b9ea695220064ce0918a524736ecd8b2f
Aravind <aravindmarripudi@gmail.com>
2025-04-06 21:14:37 +0530

    <.,..

--- backend/app.js
+++ backend/app.js
@@ -55,3 +55,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`🚀 Backend is running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("MERN Task Manager API is running 🚀");
+});
